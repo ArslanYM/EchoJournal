57c5a96202560195077fbd7013961397bec721c8
ArslanYM <arsalanyaqmalik@gmail.com>
2023-10-04 21:59:07 +0530

    feat: added db, routes, middleware to backend

--- blog-client/tailwind.config.js
+++ blog-client/tailwind.config.js
@@ -5,5 +5,5 @@ export default {
     extend: {},
   },
   // eslint-disable-next-line no-undef
-  plugins: [require("daisyui")],
+ 
 };
--- blog-server/src/index.js
+++ blog-server/src/index.js
@@ -1,1 +1,32 @@
-console.log("hello world");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+//middlewares
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// routes
+const userRouter = require("./routes/user");
+// envs
+const PORT = 3000;
+const MONGO_URL = "mongodb+srv://ars:663663@blogapp.mbksvib.mongodb.net/";
+
+//app.use
+app.use("/user", userRouter);
+
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "blog",
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on PORT ${PORT}`);
+    });
+  })
+  .catch((e) => {
+    console.error(e);
+  });
